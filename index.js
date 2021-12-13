c55bf7205372b77f8234909e7eddd0ae2d0dc0f7
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-13 09:08:52 -0500

    Add calculateDinner fn

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+const vegetarian = document.getElementById('vegetarian-input')
+const numInput = document.getElementById('num-input')
+
 const btn = document.getElementById('btn')
-let food = document.getElementById('food')
+const food = document.getElementById('food')
+
+btn.addEventListener('click', calculateDinner)
+
+function calculateDinner(e) {
+  e.preventDefault()
+  if (vegetarian.checked) {
+    food.textContent = 'nut roast'
+  } else if (numInput.value < 5) {
+    food.textContent = 'turkey'
+  } else {
+    food.textContent = 'goose'
+  }
+}
 
 // Tasks:
 // - Write the JS to decide the perfect Christmas dinner and render it in the result element. Don't forget to check whether the meal should be vegetarian!
